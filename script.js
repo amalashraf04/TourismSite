2e07a885d0661c77203eb7a43df5a0d8d861a729
amal <amalashraf04@gmail.com>
2023-02-03 14:39:50 +0530

    Form Validation 1.0

---
+++ script.js
@@ -0,0 +1,123 @@
+function validateName(){
+    let name = document.getElementById("name");
+    let nameError=document.getElementById("nameerror");
+    let regexName = /^[A-Za-z]+$/;
+    if(regexName.test(name.value)==true){
+        nameError.innerHTML="Valid Format"
+        nameError.style.color='green'
+        return true
+
+    }
+    else{
+        nameError.innerHTML="Invalid Format"
+        nameError.style.color='red'
+        return false
+    }
+}
+function validateName1(){
+    let name = document.getElementById("name1");
+    let nameError=document.getElementById("nameerror1");
+    let regexName = /^[A-Za-z]+$/;
+    if(regexName.test(name.value)==true){
+        nameError.innerHTML="Valid Format"
+        nameError.style.color='green'
+        return true
+
+    }
+    else{
+        nameError.innerHTML="Invalid Format"
+        nameError.style.color='red'
+        return false
+    }
+}
+ 
+function validatePassword(password) {
+                
+    // Do not show anything when the length of password is zero.
+    if (password.length === 0) {
+        document.getElementById("msg").innerHTML = "";
+        return;
+    }
+   
+    
+    // Create an array and push all possible values that you want in password
+    var matchedCase = new Array();
+    matchedCase.push("[$@$!%*#?&]"); // Special Charector
+    matchedCase.push("[A-Z]");      // Uppercase Alpabates
+    matchedCase.push("[0-9]");      // Numbers
+    matchedCase.push("[a-z]");     // Lowercase Alphabates
+    
+
+    // Check the conditions
+    var ctr = 0;
+    for (var i = 0; i < matchedCase.length; i++) {
+        if (new RegExp(matchedCase[i]).test(password)) {
+            ctr++;
+        }
+       
+    }
+    // Display it
+    var color = "";
+    var strength = "";
+    switch (ctr) {
+        case 0:
+        case 1:
+        case 2:
+            strength = "Very Weak";
+            color = "red";
+            break;
+        case 3:
+            strength = "Medium";
+            color = "orange";
+            break;
+        case 4:
+            strength = "Strong";
+            color = "green";
+            break;
+    }
+  
+    document.getElementById("msg").innerHTML = strength;
+    document.getElementById("msg").style.color = color;
+}
+function validatemail(){
+    let name = document.getElementById("mail");
+    let nameError=document.getElementById("mailerror");
+    let regexName = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if(regexName.test(name.value)==true){
+        nameError.innerHTML="Valid Format"
+        nameError.style.color='green'
+        return true
+
+    }
+    else{
+        nameError.innerHTML="Invalid Email Address!"
+        nameError.style.color='red'
+        return false
+    }
+}
+function validatephone(){
+    let name = document.getElementById("phone");
+    let nameError=document.getElementById("phoneerror");
+    let regexName = /^\d{10}$|^\d{3}-\d{3}-\d{4}$|^\d{3}\.\d{3}\.\d{4}$|^\d{3} \d{3} \d{4}$/;
+    if(regexName.test(name.value)==true){
+        nameError.innerHTML="Valid Format"
+        nameError.style.color='green'
+        return true
+
+    }
+    else{
+        nameError.innerHTML="Invalid Phone Number"
+        nameError.style.color='red'
+        return false
+    }
+}
+function validateconfirmPassword() {
+    var password = document.getElementById("pwd").value;
+    var confirmPassword = document.getElementById("pwd1").value;
+    if (password != confirmPassword) {
+      alert("Passwords do not match.");
+      return false;
+    }
+    return true;
+  }
+  
